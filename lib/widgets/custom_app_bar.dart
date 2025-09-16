1679d25af88f650e5005b5d68351665009fa85f2
Mario <mariomario007744@gmail.com>
2025-09-17 01:47:45 +0300

    create custome app bar

---
+++ lib/widgets/custom_app_bar.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:note_app/widgets/custom_search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Row(children:const [
+      Text("Notes",style: TextStyle(
+        fontSize: 28,
+      ),),
+      Spacer(flex: 1,),
+      CustomSearchIcon(),
+    ],);
+  }
+}
+
+
+
